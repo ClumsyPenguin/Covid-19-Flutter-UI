7cad6da6a9177a1888062623936d60d220a232b6
ClumsyPenguin <geerinckxseppe@gmail.com>
2020-08-16 20:41:06 +0200

    added refresh listerner and dropdown list

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:covid_19/widgets/my_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:http/http.dart' as http;
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import './Models/Covid.dart';
 import 'package:intl/intl.dart';
 
@@ -40,7 +41,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+  List<String> _countryList = ['Belgium', 'France', 'United States', 'Germany'];
   Future<Covid> covidData;
+  String _chosenValue;
   final controller = ScrollController();
   double offset = 0;
 
@@ -50,6 +55,21 @@ class _HomeScreenState extends State<HomeScreen> {
     covidData = fetchData();
     super.initState();
     controller.addListener(onScroll);
+  }
+
+  void _onRefresh() async {
+    // monitor network fetch
+    await fetchData();
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async {
+    // monitor network fetch
+    await fetchData();
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    if (mounted) setState(() {});
+    _refreshController.loadComplete();
   }
 
   @override
@@ -83,124 +103,139 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        controller: controller,
-        child: Column(
-          children: <Widget>[
-            MyHeader(
-              image: "assets/icons/Drcorona.svg",
-              textTop: "All you need",
-              textBottom: "is stay at home.",
-              offset: offset,
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              height: 60,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(25),
-                border: Border.all(
-                  color: Color(0xFFE5E5E5),
+      body: SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: true,
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
+        child: SingleChildScrollView(
+          controller: controller,
+          child: Column(
+            children: <Widget>[
+              MyHeader(
+                image: "assets/icons/Drcorona.svg",
+                textTop: "All you need",
+                textBottom: "is stay at home.",
+                offset: offset,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                  border: Border.all(
+                    color: Color(0xFFE5E5E5),
+                  ),
                 ),
-              ),
-              child: Row(
-                children: <Widget>[
-                  SvgPicture.asset("assets/icons/maps-and-flags.svg"),
-                  SizedBox(width: 20),
-                  Expanded(
-                    child: DropdownButton(
-                      isExpanded: true,
-                      underline: SizedBox(),
-                      icon: SvgPicture.asset("assets/icons/dropdown.svg"),
-                      value: "Belgium",
-                      items: ['Belgium', 'France', 'United States', 'Germany']
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (value) {},
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: "Case Update\n",
-                              style: kTitleTextstyle,
-                            ),
-                            TextSpan(
-                              text: DateFormat.yMMMd().format(DateTime.now()),
-                              style: TextStyle(
-                                color: kTextLightColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Spacer(),
-                      Text(
-                        "See details",
-                        style: TextStyle(
-                          color: kPrimaryColor,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          offset: Offset(0, 4),
-                          blurRadius: 30,
-                          color: kShadowColor,
-                        ),
-                      ],
-                    ),
-                    child: Container(
-                      child: FutureBuilder<Covid>(
-                        future: covidData,
-                        builder: (context, snapshot) {
-                          if (snapshot.hasData) {
-                            return DataDashboard(
-                              deaths: snapshot.data.deaths,
-                              recovered: 0,
-                              infected: snapshot.data.infected,
-                              inHospital: snapshot.data.hospitalized,
-                              tested: snapshot.data.tested,
-                              totalInHospital: snapshot.data.totalInHospital,
-                            );
-                          } else if (snapshot.hasError) {
-                            return Text("${snapshot.error}");
-                          }
-                          return CircularProgressIndicator();
+                child: Row(
+                  children: <Widget>[
+                    SvgPicture.asset("assets/icons/maps-and-flags.svg"),
+                    SizedBox(width: 20),
+                    Expanded(
+                      child: DropdownButton(
+                        isExpanded: true,
+                        underline: SizedBox(),
+                        icon: SvgPicture.asset("assets/icons/dropdown.svg"),
+                        value: _chosenValue,
+                        items: <String>[
+                          'Belgium',
+                          'France',
+                          'United States',
+                          'Germany',
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            _chosenValue = value;
+                          });
                         },
                       ),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "Case Update\n",
+                                style: kTitleTextstyle,
+                              ),
+                              TextSpan(
+                                text: DateFormat.yMMMd().format(DateTime.now()),
+                                style: TextStyle(
+                                  color: kTextLightColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          "See details",
+                          style: TextStyle(
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 4),
+                            blurRadius: 30,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Container(
+                        child: FutureBuilder<Covid>(
+                          future: covidData,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return DataDashboard(
+                                deaths: snapshot.data.deaths,
+                                recovered: 0,
+                                infected: snapshot.data.infected,
+                                inHospital: snapshot.data.hospitalized,
+                                tested: snapshot.data.tested,
+                                totalInHospital: snapshot.data.totalInHospital,
+                              );
+                            } else if (snapshot.hasError) {
+                              return Text("${snapshot.error}");
+                            }
+                            return CircularProgressIndicator();
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
